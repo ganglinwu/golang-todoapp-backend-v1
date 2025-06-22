d698079a357a63d84ecadd1377a701c29f9345d7
ganglinwu <ganglin.wu@gmail.com>
2025-06-22 09:13:42 +0800

    feat: main.go graceful shutdown

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/ganglinwu/todoapp-backend-v1/mongostore"
 	"github.com/ganglinwu/todoapp-backend-v1/server"
@@ -13,6 +18,7 @@ func main() {
 	addr := flag.String("addr", ":8080", "http address")
 
 	flag.Parse()
+
 	conn, err := mongostore.NewConnection()
 	if err != nil {
 		log.Fatal(err)
@@ -30,5 +36,29 @@ func main() {
 
 	handler := server.NewTodoServer(store)
 
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	s := http.Server{
+		Addr:              addr,
+		Handler:           handler,
+		ReadHeaderTimeout: 1 * time.Second,
+		WriteTimeout:      2 * time.Second,
+		// ErrorLog errLogger,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to listen and serve. Reason:", err.Error())
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, syscall.SIGTERM)
+
+	sig := <-sigChan
+	log.Println("received terminate, shutting down gracefully. Signal received:", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(ctx)
 }
